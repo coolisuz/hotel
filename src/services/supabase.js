aa78bc6ea94defaec1e24ef787cc0179c3841acc
Saidjamolhon Ikramov <saidjamol1@gmail.com>
2024-08-27 00:59:24 +0500

    supabase setup

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://incogpgzbhyjkwqaioof.supabase.co";
+
+// ok to expose, row level policy is turned on
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluY29ncGd6Ymh5amt3cWFpb29mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkxMjg0MTMsImV4cCI6MjAzNDcwNDQxM30.6RFYDe87T_931C3WChvAR2jevXBgqGgIXOt1YHmX2eg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
