283ab09f15f443912f8642417ed567a424c43d8a
Saidjamol <saidjamol1@gmail.com>
2024-10-05 11:26:39 +0500

    refactor modal using Compound Component pattern

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,24 +1,25 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 import Modal from "../../ui/Modal";
 
 function AddCabin() {
-  const [isOpenModal, setisOpenModalModal] = useState(false);
-
   return (
-    <div>
-      <Button
-        onClick={() => setisOpenModalModal((isOpenModal) => !isOpenModal)}
-      >
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setisOpenModalModal(false)}>
-          <CreateCabinForm onCloseModal={() => setisOpenModalModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabins-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabins-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="cabins-table">
+        <Button>Show Table</Button>
+      </Modal.Open>
+      <Modal.Window name="cabins-table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
 
