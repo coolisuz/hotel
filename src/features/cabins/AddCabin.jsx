39212a10220cd1a98e6542eb82c02a04e308418d
Saidjamol <saidjamol1@gmail.com>
2024-10-05 13:07:46 +0500

    cleanup

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,25 +1,19 @@
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
-import CabinTable from "./CabinTable";
 import Modal from "../../ui/Modal";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabins-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabins-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="cabins-table">
-        <Button>Show Table</Button>
-      </Modal.Open>
-      <Modal.Window name="cabins-table">
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabins-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabins-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
 
