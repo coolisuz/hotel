3e25ce4204be30603eb82bf3f1f96d1e4914e17c
Saidjamol <saidjamol1@gmail.com>
2024-10-07 11:06:59 +0500

    build booking table

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,17 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
+export async function getBookings() {
+  const { data, error } = await supabase.from('bookings').select('*, cabins(name), guests(*)');
+
+  if (error) {
+    console.log(error)
+    throw new Error('Bookings not found')
+  }
+
+  return data;
+}
+
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
